e3a1bcb1bbad8f084a0ccad09aaf141ff2853811
Warren Paschetto <wdpaschetto@gmail.com>
2024-12-05 14:43:38 -0500

    added game screen

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.get_init()
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        screen.fill((0, 0, 0))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return 
+                
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
